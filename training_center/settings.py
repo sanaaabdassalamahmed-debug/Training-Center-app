8bec8260e91d2f661bdc8321ec4b86d9f56515ad
Sana <sanaaabdassalamahmed@gmail.com>
2026-07-07 01:49:58 +0300

    add WhiteNoise and update middleware

--- training_center/settings.py
+++ training_center/settings.py
@@ -29,17 +29,17 @@ INSTALLED_APPS = [
 
 
 MIDDLEWARE = [
-    "corsheaders.middleware.CorsMiddleware",  # مهم جداً
-
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware', 
+    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
 
 
 ROOT_URLCONF = 'training_center.urls'
